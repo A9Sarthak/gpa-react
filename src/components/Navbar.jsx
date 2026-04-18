51be1697509ab56ef50bbf3bf503511f7e00a749
Sarthak Agarwal <sarthaka700@gmail.com>
2026-04-18 19:38:07 +0530

    brand: rename 'GPA Calc' to 'Gradevity' in UI components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
       <div className="navbar-container">
         <div className="nav-brand-clickable" onClick={handleLogoClick}>
           <GraduationCap size={32} className="text-accent" />
-          <span className="brand-text">GPA <span className="smooth-gradient-text">Calc</span></span>
+          <span className="brand-text">Grade<span className="smooth-gradient-text">vity</span></span>
         </div>
 
         <div className="nav-actions-group">
